8380abc34ac13ae3c8e41755a8c101d7795842d9
THIYANES01 <23pcs026@muthayammal.in>
2024-11-14 03:31:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    API_ID = ""  # 🤖 Same As APP_ID. Get It From My.telegram.org.
-    API_HASH = "" # 🤖 Get It From My.telegram.org.
-    BOT_TOKEN = "" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
+    API_ID = "26412923"  # 🤖 Same As APP_ID. Get It From My.telegram.org.
+    API_HASH = "0a2d1fd4d761e5bbbe001c23cb02d361" # 🤖 Get It From My.telegram.org.
+    BOT_TOKEN = "7335433385:AAELqQVxat7wATU59CF52gWUtNJlnNZW40o" # 🤖 Get It From @BotFather (Telegram) By Sending /newbot Command.
